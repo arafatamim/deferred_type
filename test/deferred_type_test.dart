80c1a08b767d4bf9043accde443089c9dc71384e
Tamim Arafat <31634638+arafatamim@users.noreply.github.com>
2022-01-01 21:02:49 +0600

    add preliminary tests

--- test/deferred_type_test.dart
+++ test/deferred_type_test.dart
@@ -1,7 +1,50 @@
-import 'package:flutter_test/flutter_test.dart';
+import 'package:test/test.dart';
+import 'package:deferred_type/deferred.dart';
 
 void main() {
-  test('add one plus one', () {
-    expect(1 + 1, 2); // TODO add tests
+  test("should handle state accordingly in when method", () {
+    Deferred<String> state = Deferred.idle();
+    expect(
+      state.when(
+        success: (a) => false,
+        error: (error, stackTrace) => false,
+        inProgress: () => false,
+        idle: () => true,
+      ),
+      true,
+    );
+
+    state = Deferred.inProgress();
+    expect(
+      state.when(
+        success: (result) => false,
+        error: (error, stackTrace) => false,
+        inProgress: () => true,
+        idle: () => false,
+      ),
+      true,
+    );
+
+    state = Deferred.success("success");
+    expect(
+      state.when(
+        success: (result) => true,
+        error: (error, stackTrace) => false,
+        inProgress: () => false,
+        idle: () => false,
+      ),
+      true,
+    );
+
+    state = Deferred.error("error", null);
+    expect(
+      state.when(
+        success: (result) => false,
+        error: (error, stackTrace) => true,
+        inProgress: () => false,
+        idle: () => false,
+      ),
+      true,
+    );
   });
 }
